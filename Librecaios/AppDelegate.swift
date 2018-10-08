286bb6a8ee9d0e41bbf8c4a7b34153727a5b8e16
Justin Marshall <jdmarshall1990@gmail.com>
2018-10-07 21:56:08 -0500

    Prototype - list of books in main table VC

--- Librecaios/AppDelegate.swift
+++ Librecaios/AppDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Justin Marshall. All rights reserved.
 //
 
+import CalibreKit
 import UIKit
 
 @UIApplicationMain
@@ -48,13 +49,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     // MARK: - Split view
 
     func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
-        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
-        guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
-        if topAsDetailController.detailItem == nil {
-            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
-            return true
-        }
-        return false
+        return true
+//        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
+//        guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
+//        if topAsDetailController.detailItem == nil {
+//            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
+//            return true
+//        }
+//        return false
     }
 
 }
