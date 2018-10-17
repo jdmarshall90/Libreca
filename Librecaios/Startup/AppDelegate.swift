5818c855c611851ca68ef6c6361d5e60a0d7a2a8
Justin Marshall <jdmarshall1990@gmail.com>
2018-10-16 20:16:05 -0500

    Remove an addressed TODO

--- Librecaios/Startup/AppDelegate.swift
+++ Librecaios/Startup/AppDelegate.swift
@@ -10,7 +10,6 @@ import CalibreKit
 import UIKit
 
 // TODO: Test on all screen sizes
-// TODO: Test tapping of "info" button on settings screen on the iPad
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
