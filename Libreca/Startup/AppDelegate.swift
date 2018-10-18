7302597d150794ebb247bf5510d87a38fb9bf6eb
Justin Marshall <jdmarshall1990@gmail.com>
2018-10-17 22:05:30 -0500

    Cleanup whitespace

--- Libreca/Startup/AppDelegate.swift
+++ Libreca/Startup/AppDelegate.swift
@@ -15,7 +15,7 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
-
+    
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
