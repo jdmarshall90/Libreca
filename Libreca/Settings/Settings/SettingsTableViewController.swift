0b1d155792f3410a972cc249c8701dc4dc429b0e
Justin Marshall <jdmarshall1990@gmail.com>
2018-12-03 17:45:33 -0600

    Add link to source code from settings

--- Libreca/Settings/Settings/SettingsTableViewController.swift
+++ Libreca/Settings/Settings/SettingsTableViewController.swift
@@ -56,6 +56,12 @@ final class SettingsTableViewController: UITableViewController, MFMailComposeVie
             
             static let credits = "Credits"
             static let licenses = "Licenses"
+            static let viewSource = "View Source Code"
+            
+            // swiftlint:disable force_unwrapping
+            // TODO: Change this to GitHub
+            static let sourceCodeSite = URL(string: "https://gitlab.com/calibre-utils/Libreca")!
+            // swiftlint:enable force_unwrapping
         }
         
         struct HeaderTitles {
@@ -140,6 +146,13 @@ final class SettingsTableViewController: UITableViewController, MFMailComposeVie
                 },
                 DisplayModel(mainText: Constants.About.licenses, subText: nil, accessoryType: .disclosureIndicator) { [weak self] in
                     self?.performSegue(withIdentifier: Segue.licensesSegue.rawValue, sender: nil)
+                },
+                DisplayModel(mainText: Constants.About.viewSource, subText: nil, accessoryType: .disclosureIndicator) { [weak self] in
+                    // TODO: Test analytics
+                    
+                    Analytics.logEvent("view_source_tapped", parameters: nil)
+                    let safariVC = SFSafariViewController(url: Constants.About.sourceCodeSite)
+                    self?.present(safariVC, animated: true)
                 }
             ]
         ]
