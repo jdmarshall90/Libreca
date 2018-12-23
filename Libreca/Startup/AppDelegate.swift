20d5385d59f6eb98f869b761db528c9dff663929
Justin Marshall <jdmarshall1990@gmail.com>
2018-12-22 23:51:46 -0600

    Remove TODO, this one also not worth the time

--- Libreca/Startup/AppDelegate.swift
+++ Libreca/Startup/AppDelegate.swift
@@ -72,7 +72,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         
         switch Settings.Theme.current {
         case .dark:
-            // TODO: test licenses screen
             UIView.appearance(whenContainedInInstancesOf: [UITableViewCell.self]).backgroundColor = #colorLiteral(red: 0.1960784314, green: 0.2156862745, blue: 0.262745098, alpha: 1)
             UILabel.appearance(whenContainedInInstancesOf: [UITableViewCell.self]).backgroundColor = #colorLiteral(red: 0.1960784314, green: 0.2156862745, blue: 0.262745098, alpha: 1)
             UIImageView.appearance().backgroundColor = .clear
