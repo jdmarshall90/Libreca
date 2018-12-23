091e92d9e017e75c11c0cef19d36ea7924bc8218
Justin Marshall <jdmarshall1990@gmail.com>
2018-12-23 17:29:51 -0600

    Fix messages VC colors when exporting data

--- Libreca/Settings/Settings/Settings.swift
+++ Libreca/Settings/Settings/Settings.swift
@@ -167,7 +167,6 @@ struct Settings {
             switch self {
             case .dark:
                 // TODO: Content server text field placeholder color
-                // TODO: Messages VC when exporting, colors
                 UILabel.appearance(whenContainedInInstancesOf: [UITableViewCell.self]).backgroundColor = #colorLiteral(red: 0.1960784314, green: 0.2156862745, blue: 0.262745098, alpha: 1)
                 UIImageView.appearance().backgroundColor = .clear
                 UITableViewCell.appearance().backgroundColor = #colorLiteral(red: 0.1960784314, green: 0.2156862745, blue: 0.262745098, alpha: 1)
