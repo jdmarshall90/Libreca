2da9b23b37916f7c6d5c7cede1eb7b14c0bdc57a
Justin Marshall <jdmarshall1990@gmail.com>
2018-12-23 16:41:21 -0600

    Add TODO

--- Libreca/Startup/AppDelegate.swift
+++ Libreca/Startup/AppDelegate.swift
@@ -63,6 +63,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         
         switch Settings.Theme.current {
         case .dark:
+            // TODO: Messages VC when exporting, colors
             UILabel.appearance(whenContainedInInstancesOf: [UITableViewCell.self]).backgroundColor = #colorLiteral(red: 0.1960784314, green: 0.2156862745, blue: 0.262745098, alpha: 1)
             UIImageView.appearance().backgroundColor = .clear
             UITableViewCell.appearance().backgroundColor = #colorLiteral(red: 0.1960784314, green: 0.2156862745, blue: 0.262745098, alpha: 1)
