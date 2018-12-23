85571b0d804246ee9ef44acfbd67e3369eef1518
Justin Marshall <jdmarshall1990@gmail.com>
2018-12-23 17:14:27 -0600

    Move app theming out of app delegate

--- Libreca/Settings/Settings/Settings.swift
+++ Libreca/Settings/Settings/Settings.swift
@@ -159,5 +159,36 @@ struct Settings {
                 NotificationCenter.default.post(didChangeNotification)
             }
         }
+        
+        func stylizeApp() {
+            // TODO: Test analytics
+            // TODO: Centralize colors into static helper struct
+            
+            switch self {
+            case .dark:
+                // TODO: Messages VC when exporting, colors
+                UILabel.appearance(whenContainedInInstancesOf: [UITableViewCell.self]).backgroundColor = #colorLiteral(red: 0.1960784314, green: 0.2156862745, blue: 0.262745098, alpha: 1)
+                UIImageView.appearance().backgroundColor = .clear
+                UITableViewCell.appearance().backgroundColor = #colorLiteral(red: 0.1960784314, green: 0.2156862745, blue: 0.262745098, alpha: 1)
+                UINavigationBar.appearance().barTintColor = #colorLiteral(red: 0.1725490196, green: 0.1764705882, blue: 0.1843137255, alpha: 1)
+                UITableView.appearance().backgroundColor = #colorLiteral(red: 0.1764705882, green: 0.1764705882, blue: 0.1764705882, alpha: 1)
+                UILabel.appearance().textColor = .white
+                UILabel.appearance().backgroundColor = .clear
+                UIActivityIndicatorView.appearance().style = .white
+                UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white]
+                UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white, .backgroundColor: #colorLiteral(red: 0.1725490196, green: 0.1764705882, blue: 0.1843137255, alpha: 1)]
+                UITextField.appearance().textColor = .white
+                UITextField.appearance().tintColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+                UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: #colorLiteral(red: 0.1960784314, green: 0.2156862745, blue: 0.262745098, alpha: 1)], for: .selected)
+                UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.white], for: .normal)
+                UISegmentedControl.appearance().backgroundColor = #colorLiteral(red: 0.1960784314, green: 0.2156862745, blue: 0.262745098, alpha: 1)
+                UISegmentedControl.appearance().tintColor = .white
+                UIButton.appearance().tintColor = .white
+            case .light:
+                // TODO: Test on all screen sizes / orientations
+                // TODO: Test on physical device
+                break
+            }
+        }
     }
 }
