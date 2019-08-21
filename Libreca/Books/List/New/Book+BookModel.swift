215aeb344fcb08ca34c42b4b476061c88db27283
Justin Marshall <jdmarshall1990@gmail.com>
2019-08-21 17:41:05 -0500

    Update text of TODO

--- Libreca/Books/List/New/Book+BookModel.swift
+++ Libreca/Books/List/New/Book+BookModel.swift
@@ -51,7 +51,7 @@ extension Book: BookModel {
     }
     
     func fetchMainFormat(completion: @escaping (Result<BookDownload, FetchError>) -> Void) {
-        // TODO: Switch from active Dropbox connection to active content server connection, and the first subsequent download doesn't show up on UI, and also causes all previous downloads to be deleted.
+        // TODO: Switch from active Dropbox connection to active content server connection (or vice versa, mostly vice versa), and the first subsequent download doesn't show up on UI, and also causes all previous downloads to be deleted. It (visually) appears to happen only after the download completes.
         
         // The interactor is expected to enforce this mainFormat being non-nil before
         // calling this function. Hence the force unwrap.
